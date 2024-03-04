7268150185b1a7012caa12b9301e414c59dbde2a
vanchess-git <72012822+vanchess-git@users.noreply.github.com>
2024-03-04 05:18:35 +0200

    incomplete calcs, might still redesign it from scratch

--- src/views/battleCalculator/BattleCalculator.tsx
+++ src/views/battleCalculator/BattleCalculator.tsx
@@ -17,147 +17,101 @@ import {plasticUnits} from "../../data/plasticUnits";
 import {PlasticFaction} from "../../data/interfaces/PlasticFaction";
 import {plasticFactions} from "../../data/plasticFactions";
 
-interface PlasticUnitCounts {
-  unitType: string;
-  unitCount: number;
-}
+interface PlasticUnitCounts {unitType: string;unitCount: number;}
 function BattleCalculator() {
 
-  let [alliedFaction, setAlliedFaction] = useState<PlasticFaction>(
-    {
-      id: "faction_arborec",
-      name: "Arborec"
-    });
-  let [hostileFaction, setHostileFaction] = useState<PlasticFaction>(
-    {
-    id: "faction_federation_of_sol",
-    name: "Federation of Sol"
-  });
+  let [alliedFaction, setAlliedFaction] = useState<PlasticFaction>({id: "faction_arborec", name: "Arborec"});
+  let [hostileFaction, setHostileFaction] = useState<PlasticFaction>({id: "faction_federation_of_sol", name: "Federation of Sol"});
   let [alliedUnits, setAlliedUnits] = useState<PlasticUnit[]>([]);
   let [hostileUnits, setHostileUnits] = useState<PlasticUnit[]>([]);
+  let [resolveCombat, setResolveCombat] = useState<boolean>(false);
   let [alliedUnitCounts, setAlliedUnitCounts] = useState<PlasticUnitCounts[]>(
     [
-      {
-        unitType: "war_sun",
-        unitCount: 0,
-      },
-      {
-        unitType: "cruiser",
-        unitCount: 0,
-      },
-      {
-        unitType: "dreadnought",
-        unitCount: 0,
-      },
-      {
-        unitType: "destroyer",
-        unitCount: 0,
-      },
-      {
-        unitType: "pds",
-        unitCount: 0,
-      },
-      {
-        unitType: "carrier",
-        unitCount: 0,
-      },
-      {
-        unitType: "fighter",
-        unitCount: 0,
-      },
-      {
-        unitType: "infantry",
-        unitCount: 0,
-      },
-      {
-        unitType: "space_dock",
-        unitCount: 0,
-      },
-      {
-        unitType: "flagship",
-        unitCount: 0,
-      },
-      {
-        unitType: "mech",
-        unitCount: 0,
-      },
-    ])
+      {unitType: "war_sun", unitCount: 0,}, {unitType: "cruiser", unitCount: 0,}, {unitType: "dreadnought", unitCount: 0,},
+      {unitType: "destroyer", unitCount: 0,}, {unitType: "pds", unitCount: 0,}, {unitType: "carrier", unitCount: 0,},
+      {unitType: "fighter", unitCount: 0,}, {unitType: "infantry", unitCount: 0,}, {unitType: "space_dock", unitCount: 0,},
+      {unitType: "flagship", unitCount: 0,}, {unitType: "mech", unitCount: 0,},])
   let [hostileUnitCounts, setHostileUnitCounts] = useState<PlasticUnitCounts[]>(
     [
-      {
-        unitType: "war_sun",
-        unitCount: 0,
-      },
-      {
-        unitType: "cruiser",
-        unitCount: 0,
-      },
-      {
-        unitType: "dreadnought",
-        unitCount: 0,
-      },
-      {
-        unitType: "destroyer",
-        unitCount: 0,
-      },
-      {
-        unitType: "pds",
-        unitCount: 0,
-      },
-      {
-        unitType: "carrier",
-        unitCount: 0,
-      },
-      {
-        unitType: "fighter",
-        unitCount: 0,
-      },
-      {
-        unitType: "infantry",
-        unitCount: 0,
-      },
-      {
-        unitType: "space_dock",
-        unitCount: 0,
-      },
-      {
-        unitType: "flagship",
-        unitCount: 0,
-      },
-      {
-        unitType: "mech",
-        unitCount: 0,
-      },
-    ])
+      {unitType: "war_sun", unitCount: 0,}, {unitType: "cruiser", unitCount: 0,}, {unitType: "dreadnought", unitCount: 0,},
+      {unitType: "destroyer", unitCount: 0,}, {unitType: "pds", unitCount: 0,}, {unitType: "carrier", unitCount: 0,},
+      {unitType: "fighter", unitCount: 0,}, {unitType: "infantry", unitCount: 0,}, {unitType: "space_dock", unitCount: 0,},
+      {unitType: "flagship", unitCount: 0,}, {unitType: "mech", unitCount: 0,},])
 
   useEffect(() => {
     let allyArray: PlasticUnit[] = plasticUnits.filter((item): item is PlasticUnit => {
-      return (
-          typeof item === "object" &&
-              item !== null &&
-              !Array.isArray(item) &&
-              "factions" in item &&
-              Array.isArray(item.factions) &&
-              item.factions.includes(alliedFaction.id)
-      )
+      return (typeof item === "object" && item !== null && !Array.isArray(item) &&
+        "factions" in item && Array.isArray(item.factions) && item.factions.includes(alliedFaction.id))
     })
-    console.log("th LOG", allyArray)
     setAlliedUnits(allyArray);
 
     let hostileArray: PlasticUnit[] = plasticUnits.filter((item): item is PlasticUnit => {
-      return (
-        typeof item === "object" &&
-        item !== null &&
-        !Array.isArray(item) &&
-        "factions" in item &&
-        Array.isArray(item.factions) &&
-        item.factions.includes(hostileFaction.id)
-      )
+      return (typeof item === "object" && item !== null && !Array.isArray(item) &&
+        "factions" in item && Array.isArray(item.factions) && item.factions.includes(hostileFaction.id))
     })
-    console.log("th LOG", hostileArray)
     setHostileUnits(hostileArray);
 
   }, [alliedFaction, hostileFaction])
+
+  useEffect(() => {
+    let alliesInCombat: PlasticUnitCounts[] = alliedUnitCounts.filter((item):item is PlasticUnitCounts => {
+      return (typeof item == "object" && item !== null && item.unitCount > 0)
+    });
+    let hostilesInCombat: PlasticUnitCounts[] = hostileUnitCounts.filter((item):item is PlasticUnitCounts => {
+      return (typeof item == "object" && item !== null && item.unitCount > 0)
+    });
+    let alliesTotal: number = alliesInCombat.reduce((total, item) => total + item.unitCount, 0);
+    let hostilesTotal: number = hostilesInCombat.reduce((total, item) => total + item.unitCount, 0);
+    // the combat loop
+    while (alliesTotal > 0 && hostilesTotal > 0) {
+
+      let allyHits: number = alliesInCombat.map((item): number => {
+        let currentShip: PlasticUnit | undefined = alliedUnits.find((item1) => item.unitType === item1.type);
+        let currentShipHits: number = 0;
+        if (
+          currentShip?.combat.normal.noOfRolls !== undefined &&
+          currentShip?.combat.normal.strength !== undefined
+        ) {
+          for (let j = 0; j < item.unitCount; j++) {
+            for (let i = 0; i < currentShip.combat.normal.noOfRolls; i++) {
+              let newRoll: number = Math.floor(Math.random() * 10) + 1;
+              if (newRoll >= currentShip.combat.normal.strength) {
+                currentShipHits += 1;
+              }
+            }
+          }
+        }
+        return currentShipHits;
+      }).reduce((total, hits) => total + hits, 0);
+
+      let hostileHits: number = hostilesInCombat.map((item): number => {
+        let currentShip: PlasticUnit | undefined = hostileUnits.find((item1) => item.unitType === item1.type);
+        let currentShipHits: number = 0;
+        if (
+          currentShip?.combat.normal.noOfRolls !== undefined &&
+          currentShip?.combat.normal.strength !== undefined
+        ) {
+          for (let j = 0; j < item.unitCount; j++) {
+            for (let i = 0; i < currentShip.combat.normal.noOfRolls; i++) {
+              let newRoll: number = Math.floor(Math.random() * 10) + 1;
+              if (newRoll >= currentShip.combat.normal.strength) {
+                currentShipHits += 1;
+              }
+            }
+          }
+        }
+        return currentShipHits;
+      }).reduce((total, hits) => total + hits, 0);
+
+      alliesTotal -= hostileHits;
+      hostilesTotal -= allyHits;
+
+      while (allyHits > 0) {
+
+      }
+    }
+  }, [resolveCombat]);
+
 
   const handleAllyChange = (event: SelectChangeEvent) => {
     let newAllyFaction: PlasticFaction | undefined = plasticFactions.find((item) => event.target.value === item.id);
@@ -176,15 +130,9 @@ function BattleCalculator() {
   const increaseAllyCount = (allyUnit: PlasticUnit, index: number) => {
     const nextAllyCount: PlasticUnitCounts[] = alliedUnitCounts.map((c, i) => {
       if (c.unitType === allyUnit.type) {
-        return {
-          unitType: c.unitType,
-          unitCount: c.unitCount + 1
-        };
+        return {unitType: c.unitType, unitCount: c.unitCount + 1};
       } else {
-        return {
-          unitType: c.unitType,
-          unitCount: c.unitCount
-        };
+        return {unitType: c.unitType, unitCount: c.unitCount};
       }
     });
     setAlliedUnitCounts(nextAllyCount);
@@ -193,15 +141,9 @@ function BattleCalculator() {
   const decreaseAllyCount = (allyUnit: PlasticUnit, index: number) => {
     const nextAllyCount: PlasticUnitCounts[] = alliedUnitCounts.map((c, i) => {
       if (c.unitType === allyUnit.type) {
-        return {
-          unitType: c.unitType,
-          unitCount: c.unitCount - 1
-        };
+        return {unitType: c.unitType, unitCount: c.unitCount - 1};
       } else {
-        return {
-          unitType: c.unitType,
-          unitCount: c.unitCount
-        };
+        return {unitType: c.unitType, unitCount: c.unitCount};
       }
     });
     setAlliedUnitCounts(nextAllyCount);
@@ -210,36 +152,22 @@ function BattleCalculator() {
   const increaseHostileCount = (index: number) => {
     const nextHostileCount: PlasticUnitCounts[] = hostileUnitCounts.map((c, i) => {
       if (c.unitType === hostileUnits[index].type) {
-        return {
-          unitType: c.unitType,
-          unitCount: c.unitCount + 1
-        };
+        return {unitType: c.unitType, unitCount: c.unitCount + 1};
       } else {
-        return {
-          unitType: c.unitType,
-          unitCount: c.unitCount
-        };
+        return {unitType: c.unitType, unitCount: c.unitCount};
       }
     });
-    console.log(nextHostileCount)
     setHostileUnitCounts(nextHostileCount);
   }
 
   const decreaseHostileCount = (index: number) => {
     const nextHostileCount: PlasticUnitCounts[] = hostileUnitCounts.map((c, i) => {
       if (c.unitType === hostileUnits[index].type) {
-        return {
-          unitType: c.unitType,
-          unitCount: c.unitCount - 1
-        };
+        return {unitType: c.unitType, unitCount: c.unitCount - 1};
       } else {
-        return {
-          unitType: c.unitType,
-          unitCount: c.unitCount
-        };
+        return {unitType: c.unitType, unitCount: c.unitCount};
       }
     });
-    console.log(nextHostileCount)
     setHostileUnitCounts(nextHostileCount);
   }
 
@@ -249,8 +177,8 @@ function BattleCalculator() {
         App is started
       </Typography>
       <BarChart
-        xAxis={[{ scaleType: 'band', data: ['Turn 1', 'Turn 2', 'Turn 3', 'Turn 4', 'Turn 5'] }]}
-        series={[{ data: [20, 36, 48.8, 59.04, 67.23] }, { data: [20, 36, 48.8, 59.04, 67.23] }]}
+        xAxis={[{ scaleType: 'band', data: ['Turn 1', 'Turn 2', 'Turn 3', 'Turn 4', 'Turn 5', ] }]}
+        series={[{ data: [20, 36, 48.8, 59.04, 67.23] }, { data: [20, 36, 48.8, 59.04, 67.23] }, ]}
         width={500}
         height={300}
       />
